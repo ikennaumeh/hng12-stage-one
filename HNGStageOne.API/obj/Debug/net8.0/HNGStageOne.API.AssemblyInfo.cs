a921d791988329fb552ec17de8a1bc5c94e0c29f
Ikenna Umeh <kennaumeh@gmail.com>
2025-02-03 09:43:04 +0100

    Added docker support

--- HNGStageOne.API/obj/Debug/net8.0/HNGStageOne.API.AssemblyInfo.cs
+++ HNGStageOne.API/obj/Debug/net8.0/HNGStageOne.API.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("15ae65af-9c00-4612-b41c-81b9522546ce")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNGStageOne.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41880da3d68a5762bda8d21c8f4d76d0de4bd3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0b5373cf5317b8af54c1098c872f27ec7edf67")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNGStageOne.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNGStageOne.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
